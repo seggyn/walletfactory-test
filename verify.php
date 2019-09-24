ab29aa1015493fdff99df7910a1b5a1f21786b7f
Andrii <anrey.pk@gmail.com>
2019-09-24 23:08:25 +0300

    Changed for task 4

--- verify.php
+++ verify.php
@@ -1,14 +1,19 @@
 <?php
 
+$PRIVATE_KEY = file_get_contents('rsa/walletfactory');
 $SALT = 'salt';
 
 $raw_post_request = file_get_contents('php://input');
 
-$data = json_decode($raw_post_request);
+$array = json_decode($raw_post_request);
+$verify_data = $array->name.$array->birthday.$array->message;
 
-$verify_hash = md5($SALT.$data->name.$data->email.$data->birthday.$data->message.$SALT);
+$data = $array->hash;
 
-if ($verify_hash == $data->hash) {
+$pk  = openssl_get_privatekey($PRIVATE_KEY);
+openssl_private_decrypt(base64_decode($data), $result, $pk);
+
+if ($verify_data == $result) {
     echo json_encode(['status' => 1, 'message' => 'Успех']);
 } else {
     echo json_encode(['status' => 0, 'message' => 'Провал']);
